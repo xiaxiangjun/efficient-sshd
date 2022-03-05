79d80468093fb4856f2f9cfaba04d419bd5059c0
xiaxiangjun <yuanjie.xia@wholeally.com>
2022-03-05 23:44:39 +0800

    windows平台增加启动msys

--- main.go
+++ main.go
@@ -3,13 +3,39 @@ package main
 import (
 	"efficient-sshd/serve"
 	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 func main() {
 	config := &serve.Config{}
 
-	flag.IntVar(&config.Port, "port", 8022, "listen tcp port");
+	flag.IntVar(&config.Port, "port", 8022, "listen tcp port")
 	flag.Parse()
+
+	// 判断是否在msys下运行
+	if runtime.GOOS == "windows" {
+		if os.Getenv("MSYSTEM") != "MSYS" {
+			exe, _ := filepath.Abs(os.Args[0])
+			exe = strings.ReplaceAll(exe, "\\", "/")
+			// 添加参数
+			for i := 1; i < len(os.Args); i++ {
+				exe += "\x20" + os.Args[i]
+			}
+
+			log.Println("start exe: ", exe)
+			// 启动子进程
+			cmd := exec.Command("C:\\msys64\\msys2_shell.cmd", "-msys", "-c", exe)
+			cmd.Env = os.Environ()
+			// 启动子进程
+			cmd.Start()
+			return
+		}
+	}
 
 	// 打印信息
 	config.Dump()
